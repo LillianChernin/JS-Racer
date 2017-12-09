a95a56942bcc308dc89db47736ccc88c5bb43d5c
Lillian Chernin <l.smith1654@gmail.com>
2017-12-09 08:42:58 -0600

    fix invocation of checkAndAnnounceWinner func, create finishLinePos and update checkAndAnnounceWinner func

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const redCar = document.getElementById('red-car');
 
 let blueCarPos = 0;
 let redCarPos = 0;
+let finishLinePos = 1150;
 
 const checkAndAnnounceWinner = () => {
-  if (redCarPos >= 1200 && blueCarPos >= 1200) {
+  if (redCarPos >= finishLinePos && blueCarPos >= finishLinePos) {
     if (redCarPos === blueCarPos) {
       alert("We have a tie!!!");
     } else if (redCarPos > blueCarPos) {
@@ -16,10 +17,10 @@ const checkAndAnnounceWinner = () => {
     } else if (blueCarPos > redCarPos) {
       alert("Blue car won!! What a nail-biting finish!!");
     }
-  } else if (redCarPos >= 1200) {
-    alert("Red car won!!!");
-  } else if (blueCarPos >= 1200) {
-    alert("Blue car won!!!");
+  } else if (redCarPos >= finishLinePos) {
+    alert("Red car is the WINNNER!!!");
+  } else if (blueCarPos >= finishLinePos) {
+    alert("Blue car is the WINNER!!!");
   }
 }
 
@@ -28,12 +29,14 @@ const mobilizeCars = () => {
     if (event.key === "k") {
       blueCarPos = blueCarPos + 25;
       blueCar.setAttribute("style", "position: relative; left: " + blueCarPos + "px");
+      checkAndAnnounceWinner();
     } else if (event.key === "s") {
       redCarPos = redCarPos + 25;
       redCar.setAttribute("style", "position: relative; left: " + redCarPos + "px");
+      checkAndAnnounceWinner();
     }
   });
-  checkAndAnnounceWinner();
+
 }
 
 mobilizeCars();
