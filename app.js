57a0f208fe991cbf04242c05f4a6b1204b1fa030
Lillian Chernin <l.smith1654@gmail.com>
2017-12-10 19:46:39 -0600

    fix 2nd game doubled speed bug

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 
 const blueCar = document.getElementById('blue-car');
-
 const redCar = document.getElementById('red-car');
-
 const startButton = document.getElementById('start-button');
-
 const count = document.getElementById('count-down');
 
 let blueCarPos = 0;
@@ -12,6 +9,11 @@ let redCarPos = 0;
 let finishLinePos = window.screen.width - 100;
 let blueCarScore = 0;
 let redCarScore = 0;
+
+//Adjust race track and finish line position to fit current window screen size
+document.getElementById('road').setAttribute("style", "width: " + window.screen.width + "px");
+document.getElementById('finish-line').setAttribute("style", "left: " + finishLinePos + "px");
+//
 
 const updateScore = () => {
   document.getElementById('score').innerHTML = "Blue score: " + blueCarScore + "  /  Red score: " + redCarScore;
@@ -23,33 +25,32 @@ const resetRace = () => {
   blueCar.setAttribute("style", "position: relative; left: " + blueCarPos + "px");
   redCar.setAttribute("style", "position: relative; left: " + redCarPos + "px");
   count.innerHTML = "3";
-  updateScore();
+  if (blueCarScore > 0 || redCarScore > 0) {
+    document.removeEventListener('keypress');
+  }
 }
 
 const checkAndAnnounceWinner = () => {
-  if (redCarPos >= finishLinePos && blueCarPos >= finishLinePos) {
-    if (redCarPos === blueCarPos) {
-      alert("We have a tie!!!");
-      document.getElementById("start-button").innerHTML = "Play Again";
-    } else if (redCarPos > blueCarPos) {
-      alert("Red car won!! What a nail-biting finish!!");
-      redCarScore++;
-      document.getElementById("start-button").innerHTML = "Play Again";
-    } else if (blueCarPos > redCarPos) {
-      alert("Blue car won!! What a nail-biting finish!!");
-      blueCarScore++;
-      document.getElementById("start-button").innerHTML = "Play Again";
-    }
+  if (redCarPos >= finishLinePos && blueCarPos >= finishLinePos && redCarPos === blueCarPos) {
+    alert("We have a tie!!!");
+    document.getElementById("start-button").innerHTML = "Play Again";
   } else if (redCarPos >= finishLinePos) {
     alert("Red car is the WINNNER!!!");
     redCarScore++;
+    updateScore();
+    document.removeEventListener('keypress');
     document.getElementById("start-button").innerHTML = "Play Again";
   } else if (blueCarPos >= finishLinePos) {
     alert("Blue car is the WINNER!!!");
     blueCarScore++;
+    updateScore();
+    document.removeEventListener('keypress');
     document.getElementById("start-button").innerHTML = "Play Again";
   }
 }
+
+
+
 
 const mobilizeCars = () => {
   document.addEventListener('keypress', (event) => {
@@ -61,15 +62,15 @@ const mobilizeCars = () => {
     } else if (event.key === "s") {
       redCarPos = redCarPos + 25;
       redCar.setAttribute("style", "position: relative; left: " + redCarPos + "px");
+      count.setAttribute("style", "color: transparent");
       checkAndAnnounceWinner();
     }
   });
-
 }
 
 const countDown = () => {
   setTimeout(() => {
-    count.setAttribute("style", "left: " + (window.screen.width / 2) + "px; color: yellow");
+    count.setAttribute("style", "left: " + ((window.screen.width / 2) - 100) + "px; color: yellow");
   }, 1000);
   setTimeout(() => {
     count.innerHTML = "2";
@@ -79,19 +80,12 @@ const countDown = () => {
   }, 3000);
   setTimeout(() => {
     count.innerHTML = "GO!!!";
+    mobilizeCars();
   }, 4000);
-  count.setAttribute("style", "color: transparent");
 }
 
-const adjustRaceTrack = () => {
-  document.getElementById('road').setAttribute("style", "width: " + window.screen.width + "px");
-  document.getElementById('finish-line').setAttribute("style", "left: " + finishLinePos + "px");
-}
-
-adjustRaceTrack();
 
 startButton.addEventListener('click', (event) => {
   resetRace();
   countDown();
-  mobilizeCars();
 });
